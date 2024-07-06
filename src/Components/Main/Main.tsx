24c54c5e6b64417af4677a6e02a33f33fd233ce9
Ghayth Sabeaallil <gs222mh@gmail.com>
2024-07-06 20:36:31 +0200

    main done

--- src/Components/Main/Main.tsx
+++ src/Components/Main/Main.tsx
@@ -1,8 +1,47 @@
+import "../../Style/Components/Main.scss";
+/*
+<img className="pic" src="/pic.jpeg" alt="pic" />
+ */
+
 const Main = () => {
     return (
-        <main>
+        <>
+            <main>
+                <div className="container">
+                    <div className="left-div">
+                        <div className="circle-shadow">
+                            <img className="pic" src="/pic.jpeg" alt="pic" />
 
-        </main>
-    )
-}
+                            <ul className="menu">
+                                <li>
+                                    <a target="_blank" rel="noopener noreferrer" href="https://github.com/Ghayth-Sabeaallil">
+                                        <img src="/github.svg" />
+                                    </a>
+                                </li>
+                                <li>
+                                    <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/ghayth-sabeaallil/">
+                                        <img src="/linkedin.svg" />
+                                    </a>
+                                </li>
+                                <li>
+                                    <a target="_blank" rel="noopener noreferrer" href="mailto:ghaessesa58@gmail.com">
+                                        <img src="/at.svg" />
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="right-div">
+                        <h1 className="name">GHAYTH SABEAALLIL</h1>
+                        <h2 className="subtext">
+                            &lt;/ <span>Front</span> <span>End</span> <span>Developer</span>{" "}
+                            &gt;
+                        </h2>
+                        <h2 className="hello">Hello World!👋</h2>
+                    </div>
+                </div>
+            </main>
+        </>
+    );
+};
 export default Main;
--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,9 +1,12 @@
 import { lazy } from 'react';
 const Header = lazy(() => import('../Components/Header/Header'));
+const Main = lazy(() => import('../Components/Main/Main'));
+
 
 function Home() {
     return (<>
         <Header />
+        <Main />
     </>);
 }
 
